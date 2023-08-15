9b06951f260d17be0ed9f30cbbd6dfa571e93fc2
Jackson-Mseven <bill714@foxmail.com>
2023-08-15 15:51:57 +0800

    修复了依赖体积的bug

--- lib/calculateDependentSize.ts
+++ lib/calculateDependentSize.ts
@@ -1,4 +1,5 @@
 const remote = require('remote-file-size')
+const need = Object.keys(require(process.cwd().replace(/\\/g, '/') + '/package.json').dependencies)
 
 /**
  * 处理 npm 包
@@ -9,7 +10,11 @@ async function getNpmSizes() {
   for (const key of Object.keys(packages)) {
     const arr = key.split('node_modules/')
     const mapKey = arr[arr.length - 1]
-    urlMap.set(mapKey + ' : ' + packages[key].version, packages[key].resolved)
+    if (need.includes(mapKey)) {
+      urlMap.set(mapKey + ' : ' + packages[key].version, packages[key].resolved)
+      need.splice(need.indexOf(mapKey), 1)
+      if (urlMap.size === need.length) break;
+    }
   }
 
   const p = new Promise((resolve: Function, reject: Function) => {
@@ -38,15 +43,19 @@ async function getYarnSizes() {
   const urlJSON = JSON.parse(fs.readFileSync('./node_modules/.yarn-integrity', 'utf8')).lockfileEntries
   const p = new Promise((resolve: Function, reject: Function) => {
     const sizeMap = new Map()
+    const len = need.length;
     Object.keys(urlJSON).forEach((item, index) => {
-      const key = item.replace(/@(\^|~)?/i, ' : ')
-      remote(urlJSON[item], (err: Error, size: number) => {
-        if (err) reject('获取依赖体积失败了')
-        sizeMap.set(key, (size / 1024).toFixed(2) + 'kB')
-        if (index === Object.keys(urlJSON).length - 1) {
-          resolve(sizeMap)
-        }
-      })
+      if (need.includes(item.split('@')[0])) {
+        const key = item.replace(/@(\^|~)?/i, ' : ')
+        remote(urlJSON[item], (err: Error, size: number) => {
+          if (err) reject('获取依赖体积失败了')
+          sizeMap.set(key, (size / 1024).toFixed(2) + 'kB')
+          if (sizeMap.size === len) {
+            resolve(sizeMap)
+          }
+        })
+        need.splice(need.indexOf(item.split('@')[0]), 1)
+      }
     })
   })
   let res
@@ -67,19 +76,24 @@ async function getPnpmSizes() {
   const arr = packages.split('tarball: ')
   const p = new Promise((resolve: Function, reject: Function) => {
     const sizeMap = new Map();
+    const len = need.length;
     for (let i = 0; i < arr.length - 1; i++) {
       const nameStartIndex = arr[i].lastIndexOf('registry.npmmirror.com/')
       const nameEndIndex = arr[i].slice(nameStartIndex).indexOf(':') + nameStartIndex
       const urlIndex = arr[i + 1].indexOf('}')
-      const name = arr[i].slice(nameStartIndex + 23, nameEndIndex).replace(/@(\^|~)?/i, ' : ') // 包名
-      const url = arr[i + 1].slice(0, urlIndex) // 地址
-      remote(url, (err: Error, size: number) => {
-        if (err) reject('获取依赖体积失败了')
-        sizeMap.set(name, (size / 1024).toFixed(2) + 'kB')
-        if (i === arr.length - 2) {
-          resolve(sizeMap)
-        }
-      })
+      const nameNoVersion = arr[i].slice(nameStartIndex + 23, nameEndIndex).split('@')[0]
+      if (need.includes(nameNoVersion)) {
+        const name = arr[i].slice(nameStartIndex + 23, nameEndIndex).replace(/@(\^|~)?/i, ' : ') // 包名
+        const url = arr[i + 1].slice(0, urlIndex) // 地址
+        remote(url, (err: Error, size: number) => {
+          if (err) reject('获取依赖体积失败了')
+          sizeMap.set(name, (size / 1024).toFixed(2) + 'kB')
+          if (sizeMap.size === len) {
+            resolve(sizeMap)
+          }
+        })
+        need.splice(need.indexOf(nameNoVersion), 1)
+      }
     }
   })
   let res
