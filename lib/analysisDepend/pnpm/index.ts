9aa80ee94a73deae8b5483faea7bd16183f63fef
xun-zi <xun-zi@1228304333@qq.com>
2023-08-21 22:17:14 +0800

    对pnpm不同锁版本进行兼容

--- lib/analysisDepend/pnpm/index.ts
+++ lib/analysisDepend/pnpm/index.ts
@@ -2,7 +2,7 @@ import * as yaml from 'js-yaml';
 import * as fs from 'fs';
 import * as type from './type';
 import path = require('path');
-import { getJsonFileObjPath, nameVersionStringify } from '../utils';
+import { nameVersionStringify } from '../utils';
 import field from '../field';
 import { DenpendType, DependHash } from '../type';
 function getPnpmYamlObj() {
@@ -14,11 +14,14 @@ function getPnpmYamlObj() {
 		const PnpmYamlObj = yaml.load(fs.readFileSync(pnpm_yamlPath, 'utf-8'));
 		return PnpmYamlObj as type.pnpmLockYamlObj;
 	} catch (e) {
-		console.log(e);
+		throw new Error('pnpm-lock.yaml解析失败');
 	}
 }
 
-function getPnpmAllDependHash(depnedType: DenpendType = field.dependencies) {
+function getPnpm5_4AllDependHash(
+	depnedType: DenpendType,
+	yamlObj: type.pnpmLockYaml5_4Obj
+) {
 	function dependenciesVersionFormat(
 		dependencies: Record<string, string> | undefined
 	) {
@@ -28,8 +31,7 @@ function getPnpmAllDependHash(depnedType: DenpendType = field.dependencies) {
 		});
 		return res;
 	}
-	const yamlObj = getPnpmYamlObj();
-	const hash: Record<string, Record<string, string>> = {};
+	const hash: DependHash = {};
 	Object.entries(yamlObj?.packages || {}).forEach(([key, value]) => {
 		const arr = key.split('/');
 		//删除第一个空字符
@@ -48,6 +50,48 @@ function getPnpmAllDependHash(depnedType: DenpendType = field.dependencies) {
 	const nameVersion = nameVersionStringify(entryPack.name, entryPack.version);
 	hash[nameVersion] = dependenciesVersionFormat(yamlObj?.[depnedType] || {});
 	return hash;
+}
+function getPnpm6_0AllDependHash(
+	depnedType: DenpendType = field.dependencies,
+	yamlObj: type.pnpmLockYaml6_0Obj
+) {
+	function dependenciesVersionFormat(
+		dependencies: Record<string, string> | undefined
+	) {
+		const res: Record<string, string> = {};
+		Object.entries(dependencies || {}).forEach(([name, version]) => {
+			res[name] = version.split('@').pop()!;
+		});
+		return res;
+	}
+	const hash: DependHash = {};
+	Object.entries(yamlObj.packages).forEach(([packName, info]) => {
+		hash[nameVersionStringify(packName, info.version)] =
+			dependenciesVersionFormat(info.dependencies);
+	});
+	//获取用户包的名字
+	const entryPack = require(path.join(
+		process.cwd().replace(/\\/g, '/'),
+		'./package.json'
+	));
+	const nameVersion = nameVersionStringify(entryPack.name, entryPack.version);
+	const userDepend: Record<string, string> = {};
+	Object.entries(yamlObj[depnedType] || {}).forEach(([packName, info]) => {
+		const { version } = info;
+		userDepend[packName] = version;
+	});
+	hash[nameVersion] = dependenciesVersionFormat(userDepend);
+	return hash;
+}
+
+function getPnpmAllDependHash(depnedType: DenpendType = field.dependencies) {
+	const yamlObj = getPnpmYamlObj();
+	const exeHash = {
+		'5.4': getPnpm5_4AllDependHash,
+		'6.0': getPnpm6_0AllDependHash,
+	};
+	//@ts-ignore
+	return exeHash[yamlObj?.lockfileVersion]!(depnedType, yamlObj);
 }
 
 function getPnpmDependHash(/** 递归的最大深度*/ d: number) {
--- lib/analysisDepend/pnpm/type.ts
+++ lib/analysisDepend/pnpm/type.ts
@@ -1,13 +1,46 @@
+import { type } from 'os';
 import field from '../field';
 
 export type Dependencies = {};
 
-export type pnpmLockYamlObj = {
-	[field.dependencies]: Record<string, string>;
-	[field.devDependencies]: Record<string, string>;
+export enum LockFileVersion {
+	V5_4 = '5.4',
+	V6_0 = '6.0',
+}
+
+export type pnpmLockYaml5_4Obj = {
+	lockfileVersion: LockFileVersion.V5_4;
+	[field.dependencies]?: Record<string, string>;
+	[field.devDependencies]?: Record<string, string>;
 	packages: {
 		[key: string]: {
 			[field.dependencies]: Record<string, string>;
 		};
 	};
 };
+
+export type pnpmLockYaml6_0Obj = {
+	lockfileVersion: LockFileVersion.V6_0;
+	[field.dependencies]?: Record<
+		string,
+		{
+			version: string;
+		}
+	>;
+	[field.devDependencies]?: Record<
+		string,
+		{
+			version: string;
+		}
+	>;
+	packages: Record<
+		string,
+		{
+			[field.dependencies]?: Record<string, string>;
+			[field.devDependencies]?: Record<string, string>;
+			version: string;
+		}
+	>;
+};
+
+export type pnpmLockYamlObj = pnpmLockYaml5_4Obj | pnpmLockYaml6_0Obj;
