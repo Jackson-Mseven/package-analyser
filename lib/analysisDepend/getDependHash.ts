4cd336e56c02577fdee5528bed36b382de6b643f
huanxiaomang <atri2022@163.com>
2023-08-25 09:09:42 +0800

    完成单元测试

--- lib/analysisDepend/getDependHash.ts
+++ lib/analysisDepend/getDependHash.ts
@@ -6,14 +6,16 @@ import { isNumberStr } from './utils';
 import getYarnDependHash from './yarn';
 
 export default function (depth: string, packageManagementTools: string) {
-	let d: number;
-	if (!isNumberStr(depth) || depth == 'Infinity') d = 1e9;
-	else d = +depth;
+    let d: number;
+    if (!isNumberStr(depth) || depth == 'Infinity') d = 1e9;
+    else d = +depth;
 
-	const exeHash: Record<string, (d: number) => [DependHash, DependHash]> = {
-		pnpm: getPnpmDependHash,
-		npm: getNpmDependHash,
-		yarn: getYarnDependHash,
-	};
-	return exeHash[packageManagementTools](d);
+    const exeHash: Record<string, (d: number) => [DependHash, DependHash]> = {
+        pnpm: getPnpmDependHash,
+        npm: getNpmDependHash,
+        yarn: getYarnDependHash,
+    };
+
+
+    return exeHash[packageManagementTools](d);
 }
--- lib/analysisDepend/pnpm/index.ts
+++ lib/analysisDepend/pnpm/index.ts
@@ -6,125 +6,125 @@ import { nameVersionStringify } from '../utils';
 import field from '../field';
 import { DenpendType, DependHash } from '../type';
 function getPnpmYamlObj() {
-	const pnpm_yamlPath = path.join(
-		process.cwd().replace(/\\/g, '/'),
-		'/pnpm-lock.yaml'
-	);
-	try {
-		const PnpmYamlObj = yaml.load(fs.readFileSync(pnpm_yamlPath, 'utf-8'));
-		return PnpmYamlObj as type.pnpmLockYamlObj;
-	} catch (e) {
-		throw new Error('pnpm-lock.yaml解析失败');
-	}
+    const pnpm_yamlPath = path.join(
+        process.cwd().replace(/\\/g, '/'),
+        '/pnpm-lock.yaml'
+    );
+    try {
+        const PnpmYamlObj = yaml.load(fs.readFileSync(pnpm_yamlPath, 'utf-8'));
+        return PnpmYamlObj as type.pnpmLockYamlObj;
+    } catch (e) {
+        throw new Error('pnpm-lock.yaml解析失败');
+    }
 }
 
 function getPnpm5_4AllDependHash(
-	depnedType: DenpendType,
-	yamlObj: type.pnpmLockYaml5_4Obj
+    depnedType: DenpendType,
+    yamlObj: type.pnpmLockYaml5_4Obj
 ) {
-	function dependenciesVersionFormat(
-		dependencies: Record<string, string> | undefined
-	) {
-		const res: Record<string, string> = {};
-		Object.entries(dependencies || {}).forEach(([name, version]) => {
-			res[name] = version.split('_')[0];
-		});
-		return res;
-	}
-	const hash: DependHash = {};
-	Object.entries(yamlObj?.packages || {}).forEach(([key, value]) => {
-		const arr = key.split('/');
-		//删除第一个空字符
-		arr.shift();
-		const version = arr.pop()!.split('_')[0];
-		const name = arr.join('/');
-		hash[nameVersionStringify(name, version)] = dependenciesVersionFormat(
-			value.dependencies
-		);
-	});
-	//获取用户包的名字
-	const entryPack = require(path.join(
-		process.cwd().replace(/\\/g, '/'),
-		'./package.json'
-	));
-	const nameVersion = nameVersionStringify(entryPack.name, entryPack.version);
-	hash[nameVersion] = dependenciesVersionFormat(yamlObj?.[depnedType] || {});
-	return hash;
+    function dependenciesVersionFormat(
+        dependencies: Record<string, string> | undefined
+    ) {
+        const res: Record<string, string> = {};
+        Object.entries(dependencies || {}).forEach(([name, version]) => {
+            res[name] = version.split('_')[0];
+        });
+        return res;
+    }
+    const hash: DependHash = {};
+    Object.entries(yamlObj?.packages || {}).forEach(([key, value]) => {
+        const arr = key.split('/');
+        //删除第一个空字符
+        arr.shift();
+        const version = arr.pop()!.split('_')[0];
+        const name = arr.join('/');
+        hash[nameVersionStringify(name, version)] = dependenciesVersionFormat(
+            value.dependencies
+        );
+    });
+    //获取用户包的名字
+    const entryPack = require(path.join(
+        process.cwd().replace(/\\/g, '/'),
+        './package.json'
+    ));
+    const nameVersion = nameVersionStringify(entryPack.name, entryPack.version);
+    hash[nameVersion] = dependenciesVersionFormat(yamlObj?.[depnedType] || {});
+    return hash;
 }
 function getPnpm6_0AllDependHash(
-	depnedType: DenpendType = field.dependencies,
-	yamlObj: type.pnpmLockYaml6_0Obj
+    depnedType: DenpendType = field.dependencies,
+    yamlObj: type.pnpmLockYaml6_0Obj
 ) {
-	function dependenciesVersionFormat(
-		dependencies: Record<string, string> | undefined
-	) {
-		const res: Record<string, string> = {};
-		Object.entries(dependencies || {}).forEach(([name, version]) => {
-			res[name] = version.split('@').pop()!;
-		});
-		return res;
-	}
-	const hash: DependHash = {};
-	Object.entries(yamlObj.packages).forEach(([packLink, info]) => {
-		if (/^registry.npmmirror.com\//.test(packLink)) packLink.slice(22);
-		const arr = packLink.split('@');
-		const version = arr.pop()!;
-		const packName = arr.join('@').slice(1);
-		hash[nameVersionStringify(packName, version)] = dependenciesVersionFormat(
-			info.dependencies
-		);
-	});
-	//获取用户包的名字
-	const entryPack = require(path.join(
-		process.cwd().replace(/\\/g, '/'),
-		'./package.json'
-	));
-	const nameVersion = nameVersionStringify(entryPack.name, entryPack.version);
-	const userDepend: Record<string, string> = {};
-	Object.entries(yamlObj[depnedType] || {}).forEach(([packName, info]) => {
-		const { version } = info;
-		userDepend[packName] = version;
-	});
-	hash[nameVersion] = dependenciesVersionFormat(userDepend);
-	return hash;
+    function dependenciesVersionFormat(
+        dependencies: Record<string, string> | undefined
+    ) {
+        const res: Record<string, string> = {};
+        Object.entries(dependencies || {}).forEach(([name, version]) => {
+            res[name] = version.split('@').pop()!;
+        });
+        return res;
+    }
+    const hash: DependHash = {};
+    Object.entries(yamlObj.packages).forEach(([packLink, info]) => {
+        if (/^registry.npmmirror.com\//.test(packLink)) packLink.slice(22);
+        const arr = packLink.split('@');
+        const version = arr.pop()!;
+        const packName = arr.join('@').slice(1);
+        hash[nameVersionStringify(packName, version)] = dependenciesVersionFormat(
+            info.dependencies
+        );
+    });
+    //获取用户包的名字
+    const entryPack = require(path.join(
+        process.cwd().replace(/\\/g, '/'),
+        './package.json'
+    ));
+    const nameVersion = nameVersionStringify(entryPack.name, entryPack.version);
+    const userDepend: Record<string, string> = {};
+    Object.entries(yamlObj[depnedType] || {}).forEach(([packName, info]) => {
+        const { version } = info;
+        userDepend[packName] = version;
+    });
+    hash[nameVersion] = dependenciesVersionFormat(userDepend);
+    return hash;
 }
 
 function getPnpmAllDependHash(depnedType: DenpendType = field.dependencies) {
-	const yamlObj = getPnpmYamlObj();
-	const exeHash = {
-		'5.4': getPnpm5_4AllDependHash,
-		'6.0': getPnpm6_0AllDependHash,
-	};
-	//@ts-ignore
-	return exeHash[yamlObj?.lockfileVersion]!(depnedType, yamlObj);
+    const yamlObj = getPnpmYamlObj();
+    const exeHash = {
+        '5.4': getPnpm5_4AllDependHash,
+        '6.0': getPnpm6_0AllDependHash,
+    };
+    //@ts-ignore
+    return exeHash[yamlObj?.lockfileVersion]!(depnedType, yamlObj);
 }
 
 function getPnpmDependHash(/** 递归的最大深度*/ d: number) {
-	const AllDependHash = getPnpmAllDependHash(field.dependencies);
-	const AllDevDependHash = getPnpmAllDependHash(field.devDependencies);
-	const entryPack = require(path.join(
-		process.cwd().replace(/\\/g, '/'),
-		'./package.json'
-	));
+    const AllDependHash = getPnpmAllDependHash(field.dependencies);
+    const AllDevDependHash = getPnpmAllDependHash(field.devDependencies);
+    const entryPack = require(path.join(
+        process.cwd().replace(/\\/g, '/'),
+        './package.json'
+    ));
 
-	function dfs(
-		depth: number,
-		nameVersion: string,
-		allHash: DependHash,
-		hash: DependHash
-	) {
-		if (depth >= d || hash[nameVersion]) return;
-		hash[nameVersion] = allHash[nameVersion];
-		Object.entries(hash[nameVersion] || {}).forEach(([name, version]) => {
-			dfs(depth + 1, nameVersionStringify(name, version), allHash, hash);
-		});
-	}
-	const nameVersion = nameVersionStringify(entryPack.name, entryPack.version);
-	const hash: DependHash = {};
-	const devHash: DependHash = {};
-	dfs(0, nameVersion, AllDependHash, hash);
-	dfs(0, nameVersion, AllDevDependHash, devHash);
-	return [hash, devHash] as [DependHash, DependHash];
+    function dfs(
+        depth: number,
+        nameVersion: string,
+        allHash: DependHash,
+        hash: DependHash
+    ) {
+        if (depth >= d || hash[nameVersion]) return;
+        hash[nameVersion] = allHash[nameVersion];
+        Object.entries(hash[nameVersion] || {}).forEach(([name, version]) => {
+            dfs(depth + 1, nameVersionStringify(name, version), allHash, hash);
+        });
+    }
+    const nameVersion = nameVersionStringify(entryPack.name, entryPack.version);
+    const hash: DependHash = {};
+    const devHash: DependHash = {};
+    dfs(0, nameVersion, AllDependHash, hash);
+    dfs(0, nameVersion, AllDevDependHash, devHash);
+    return [hash, devHash] as [DependHash, DependHash];
 }
 
 export default getPnpmDependHash;
